972dd6c7b6f29c3132165dc02c57513d7a8880b2
AlvisonHunterArnuero <alvison@gmail.com>
2025-06-23 19:26:32 -0600

    feat(layoutResources): add welcome1 image to random backgrounds for enhanced visual variety

--- src/config/content/layoutResources.ts
+++ src/config/content/layoutResources.ts
@@ -1,5 +1,6 @@
 export const randomBackgrounds = [
     '/starwars/welcome.jpeg',
+    '/starwars/welcome1.jpeg',
     '/starwars/welcome2.jpg',
     '/starwars/welcome3.png',
     '/starwars/welcome4.png',
